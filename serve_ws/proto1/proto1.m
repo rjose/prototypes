da69aaef812ebff5d25eb55caf5d7e0571d15a87
Rino Jose <rjose@linkedin.com>
2013-05-23 13:29:38 -0700

    Changed to using char arrays instead of NSString

--- serve_ws/proto1/proto1.m
+++ serve_ws/proto1/proto1.m
@@ -21,11 +21,12 @@
  * static declarations
  */
 
+static char m_wsMagicString[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
 /*
  * Implementation
  */
-#define BUF_LENGTH 256
+#define BUF_LENGTH 200
 
 
 /*
@@ -40,33 +41,28 @@ int main()
 	char sha_digest[SHA_DIGEST_LENGTH];
 	char buf[BUF_LENGTH];
 
-	/* Magic web socket string */
-	NSString *wsMagicString = @"258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-
 	/* Comes from Sec-WebSocket-Accept */
-	NSString *key = @"dGhlIHNhbXBsZSBub25jZQ==";
+	char key[] = "dGhlIHNhbXBsZSBub25jZQ==";
+	char expected[] = "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=";
 
 	/* Concatenate the magic string, take the SHA1 and then base64 encode */
-	NSMutableString *responseValue = [NSMutableString stringWithString:key];
-	[responseValue appendString:wsMagicString];
-
-	[responseValue getCString:buf maxLength:BUF_LENGTH encoding:NSUTF8StringEncoding];
-	if ([responseValue length] >= BUF_LENGTH)
-		err(1, "Problem storing string");
-
-	SHA1((const unsigned char*)buf, [responseValue length], (unsigned char*)sha_digest);
+	strncpy(buf, key, BUF_LENGTH/2);
+	strncat(buf, m_wsMagicString, BUF_LENGTH/2);
+	SHA1((const unsigned char*)buf, strlen(buf), (unsigned char*)sha_digest);
 
 	/* Correct up to here */
 	NSData *data = [NSData dataWithBytes:sha_digest length:SHA_DIGEST_LENGTH];
 	NSData *encodedData = [GSMimeDocument encodeBase64:data];
 	[encodedData getBytes:(void*)buf length:BUF_LENGTH-1];
 	buf[[encodedData length]] = '\0'; 	/* Terminate string */
-	
 
 	NSLog(@"key: %s", buf);
 
-	/* TODO: Check the expected response */
-	//NSString *expectedResponse = @"s3pPLMBiTxaQ9kYGzzhZRbK+xOo=";
+	/* Check the expected response */
+	if (strcmp(expected, buf) == 0)
+		NSLog(@"Matches!");
+	else
+		NSLog(@"You LOSE!");
 
 	[pool release];
 	return 0;
