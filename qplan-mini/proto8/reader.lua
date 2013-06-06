62fe648801e46b0d3f71038179a74b53a1dbbbbf
Rino Jose <rjose@linkedin.com>
2013-06-06 15:26:33 -0700

    Can read work from a file

--- qplan-mini/proto8/reader.lua
+++ qplan-mini/proto8/reader.lua
@@ -1,8 +1,24 @@
 require 'string_utils'
 
 local Plan = require 'plan'
+local Work = require 'work'
 
 local Reader = {}
+
+function construct_objects_from_file(filename, constructor)
+	local result = {}
+	local file = assert(io.open(filename, "r"))
+	local cur_line = 1
+
+	for line in file:lines() do
+		-- Skipping first two header lines
+		if cur_line > 2 then
+			result[#result+1] = constructor(line)
+		end
+		cur_line = cur_line + 1
+	end
+	return result
+end
 
 function construct_plan(str)
 	local id, name, num_weeks, team_id, cutline, work_items_str = 
@@ -23,19 +39,32 @@ function construct_plan(str)
 	return result
 end
 
-function Reader.read_plans(filename)
-	local result = {}
-	local file = assert(io.open(filename, "r"))
-	local cur_line = 1
 
-	for line in file:lines() do
-		-- Skipping first two header lines
-		if cur_line > 2 then
-			result[#result+1] = construct_plan(line)
-		end
-		cur_line = cur_line + 1
+function Reader.read_plans(filename)
+	return construct_objects_from_file(filename, construct_plan)
+end
+
+function construct_work(str)
+	local id, name, track, estimate_str = unpack(str:split("\t"))
+
+	local estimates = {}
+	for _, s in pairs(estimate_str:split(", ")) do
+		-- At this point, s will be something like "Native:L"
+		local skill, est = unpack(s:split(":"))
+		estimates[skill] = est
 	end
+
+	local result = Work.new{
+		id = id,
+		name = name,
+		track = track,
+		estimates = estimates
+	}
 	return result
+end
+
+function Reader.read_work(filename)
+	return construct_objects_from_file(filename, construct_work)
 end
 
 return Reader
--- qplan-mini/proto8/tests.lua
+++ qplan-mini/proto8/tests.lua
@@ -13,5 +13,18 @@ function TestReader:test_readPlan()
 	end
 end
 
+function TestReader:test_readWork()
+	local expected_names = {"Do work item 1", "Do work item 2"}
+	local work = Reader.read_work("work.txt")
+
+	assertEquals(#work, 2)
+	for i = 1,#expected_names do
+		assertEquals(work[i].name, expected_names[i])
+	end
+
+	local estimates = work[1]:week_estimates()
+	assertEquals(estimates["Web"], 2)
+end
+
 
 LuaUnit:run()
