9168b55f2665636390d9301e80bdd4e50a7a25c9
Rino Jose <rjose@linkedin.com>
2013-06-12 13:09:22 -0700

    Can start a pthread from lua

--- lua_server/proto3/proto3.c
+++ lua_server/proto3/proto3.c
@@ -1,16 +1,41 @@
 #include <lua.h>
 #include <lauxlib.h>
 
-#include <math.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
-static int l_sin(lua_State *L) {
-	double d = lua_tonumber(L, 1); /* Get argument */
-	lua_pushnumber(L, sin(d));
-	return 1; /* Number of results */
+static pthread_t listener_thread_id;
+
+static void *listener_routine(void *arg)
+{
+	lua_State *L = (lua_State*) arg;
+	printf("%p: OUCH!\n", L);
+	while(1) {
+		fprintf(stderr, "Listening...\n");
+		sleep(5);
+	}
+	return NULL;
+}
+
+static int l_start_listening(lua_State *L) {
+	fprintf(stderr, "TODO: Start listening now\n");
+
+	lua_State *L1 = luaL_newstate();
+
+	if (!L1)
+		luaL_error(L, "Unable to create new state");
+
+	if (pthread_create(&listener_thread_id, NULL, listener_routine, L1) != 0)
+		fprintf(stderr, "Unable to create listener thread\n");
+
+	pthread_detach(listener_thread_id);
+	return 0;
 }
 
 static const struct luaL_Reg mylib [] = {
-	{"mysin", l_sin},
+	{"start_listening", l_start_listening},
 	{NULL, NULL}
 };
 
