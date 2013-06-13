12be285b5d524e268dc17c170543bddf32e97fb4
Rino Jose <rjose@ejorp.com>
2013-06-12 22:30:02 -0700

    Finished proto4 and was able to send an HTTP response in a thread

--- lua_shell/proto4/listen.m
+++ lua_shell/proto4/listen.m
@@ -1,3 +1,4 @@
+#import <Foundation/Foundation.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -23,7 +24,6 @@
 /*
  * static declarations
  */
-static void write_string(int, const char *);
 
 
 /*
@@ -31,17 +31,51 @@ static void write_string(int, const char *);
  */
 
 static void
-write_string(int connfd, const char *string)
+write_string(int connfd, NSString *string)
 {
-	Writen(connfd, string, strlen(string));
+	Writen(connfd, [string cString], [string cStringLength]);
 }
-/* TODO: Move this to its own function */
 
+/* TODO: Move this to its own function */
 static void err_abort(int status, const char *message)
 {
 	fprintf(stderr, message);
 	exit(status);
 }
+
+/*
+ * This takes an HTML response body and sends it along with the other response
+ * headers.
+ */
+static void
+res_send(int connfd, NSString *body)
+{
+	NSString *content_length =
+		[NSString stringWithFormat:@"Content-Length: %d\r\n",
+		[body cStringLength]];
+
+	write_string(connfd, @"HTTP/1.1 200 OK\r\n");
+	write_string(connfd, content_length);
+	write_string(connfd, @"Content-Type: text/html\r\n");
+	write_string(connfd, @"\r\n");
+	write_string(connfd, body);
+}
+
+static void
+handle_http_request(int connfd)
+{
+	char buf[MAXLINE];
+
+	while (readline(connfd, buf, MAXLINE) > 0) {
+		if (strcmp(buf, "\r\n") == 0)
+			break;
+		printf("Got: '%s'\n", buf);
+	}
+
+	/* Just send a basic response */
+	res_send(connfd, @"<html><body>Hi</body></html>\r\n");
+}
+
 pthread_t listener_thread_id;
 
 static void *listen_routine(void *arg)
@@ -50,7 +84,8 @@ static void *listen_routine(void *arg)
 	socklen_t clilen;
 	struct sockaddr_in cliaddr, servaddr;
         int option = 1;
-	char buf[MAXLINE];
+
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
 	//lua_State *L = (lua_State*) arg;
 
@@ -78,9 +113,10 @@ static void *listen_routine(void *arg)
 	while(1) {
 		clilen = sizeof(cliaddr);
 		connfd = accept(listenfd, (SA*) &cliaddr, &clilen);
-                printf("Got connection: %d and closing it\n", connfd);
+                handle_http_request(connfd);
                 close(connfd);
 	}
+	[pool release];
         return NULL;
 }
 
