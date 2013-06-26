add72db980aa6ff6837cfc14ce16e035ece10f45
Rino Jose <rjose@linkedin.com>
2013-06-26 16:28:55 -0700

    Rendered data to webpage

--- qplan-c/proto4/app/web.lua
+++ qplan-c/proto4/app/web.lua
@@ -1,9 +1,17 @@
 local Web = {}
 
 function Web.handle_request(req_str)
-        print(string.format("Got this string: %s", req_str))
+        -- Write some unit tests to parse this
+        -- GET /favicon.ico HTTP/1.1
+        -- Host: localhost:8888
+        -- Connection: keep-alive
+        -- Accept: */*
+        -- User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36
+        -- Accept-Encoding: gzip,deflate,sdch
+        -- Accept-Language: en-US,en;q=0.8
+        -- Cookie: visit="v=1&G"
 
-        content = "<html><body>This will be customized</body></html>\r\n"
+        content = string.format("<html><body>Cutline: %d</body></html>\r\n", pl.cutline)
         result = string.format("HTTP/1.1 200 OK\r\nContent-Length: %d\r\nContent-Type: text/html\r\n\r\n%s",
                 content:len(), content)
         return result
