4afde1237935e336316353ffbde658d559bf52be
Rino Jose <rjose@linkedin.com>
2013-07-09 10:05:57 -0700

    Added verse

---
+++ verse/proto1/proto1.lua
@@ -0,0 +1,20 @@
+local Object = require('object')
+local func = require('functional')
+require("string_utils")
+
+
+-- I assume we'll start with an array of goals and how we think various
+-- initiatives will impact them.
+--
+-- At the exec level, there are key metrics that all of the teams need to focus
+-- on. I think there are 5 key metrics (like revenue, growth, engagement, etc).
+-- 
+-- Do we need to normalize these metrics under a single value? It would be nice.
+-- I'm not sure you can get a clean function out of this. One way to start would
+-- be to have the metrics as of today vs the metrics that we want to get to over
+-- the next several quarters. For that combination of metrics, we might be able
+-- to construct some estimate of value to the company.
+--
+
+
+print "Howdy!"
