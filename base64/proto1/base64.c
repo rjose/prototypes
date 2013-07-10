0440bedaff214adad1e6991e41d62c45ac8ac298
Rino Jose <rjose@linkedin.com>
2013-07-10 15:06:33 -0700

    Can decode base64 strings

--- base64/proto1/base64.c
+++ base64/proto1/base64.c
@@ -8,6 +8,9 @@
 
 #include "base64.h"
 
+static int calcDecodeLength(const char *);
+
+
 
 int base64_encode(char **dst, const char* src)
 {
@@ -26,5 +29,41 @@ int base64_encode(char **dst, const char* src)
          BIO_free_all(bio);
          fclose(stream);
          
-         return 0; //success
+         return 0;
 }
+
+ 
+static int calcDecodeLength(const char* b64input)
+{
+        int len = strlen(b64input);
+        int padding = 0;
+
+        if (b64input[len-1] == '=' && b64input[len-2] == '=') //last two chars are =
+                padding = 2;
+        else if (b64input[len-1] == '=') //last char is =
+                padding = 1;
+
+        return (int)len*0.75 - padding;
+}
+ 
+int base64_decode(char **dst, const char *b64_src)
+{
+        BIO *bio, *b64;
+        int decodeLen = calcDecodeLength(b64_src),
+            len = 0;
+        *dst = (char*)malloc(decodeLen+1);
+        FILE* stream = fmemopen(b64_src, strlen(b64_src), "r");
+
+        b64 = BIO_new(BIO_f_base64());
+        bio = BIO_new_fp(stream, BIO_NOCLOSE);
+        bio = BIO_push(b64, bio);
+        BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Do not use newlines to flush dst
+        len = BIO_read(bio, *dst, strlen(b64_src));
+        //Can test here if len == decodeLen - if not, then return an error
+        (*dst)[len] = '\0';
+
+        BIO_free_all(bio);
+        fclose(stream);
+
+        return 0;
+} 
--- base64/proto1/tests/test1.m
+++ base64/proto1/tests/test1.m
@@ -10,12 +10,12 @@
 int main()
 {
         char *base64Output;
+        char *decodedOutput;
 
         /*
          * Test encode
          */
         base64_encode(&base64Output, "Hello");
-        printf(base64Output);
         pass(0 == strcmp(base64Output, "SGVsbG8="), "Encode hello");
 
         if (base64Output)
@@ -24,5 +24,8 @@ int main()
         /*
          * Test decode
          */
+        base64_decode(&decodedOutput, "SGVsbG8=");
+        pass(0 == strcmp(decodedOutput, "Hello"), "Decode hello");
+
         return 0;
 }
