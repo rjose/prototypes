27d4c2de8aa765e21fcc1b3183a449ce068c03fc
Rino Jose <rjose@ejorp.com>
2013-07-12 08:04:50 -0700

    Cleaned up code and documented

--- base64/proto3/base64.c
+++ base64/proto3/base64.c
@@ -8,8 +8,6 @@ const static char digits64[] =
              "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 ;
 
-static char delete_me[] = "FPucA9l+";
-
 
 int base64_encode(char **dst, const uint8_t *src, size_t len)
 {
@@ -17,69 +15,96 @@ int base64_encode(char **dst, const uint8_t *src, size_t len)
         size_t res_index;
         size_t pad_length;
         size_t result_len;
+        uint8_t cur, leftover;
         uint8_t *result;
-        // TODO: Describe this algo
-        uint8_t tmp1, cur, tmp2;
 
         /*
-         * Figure out padding and result length
+         * The pad length will be 0, 1, or 2 depending on how the final bits in
+         * the source data lay over the 6-bit encoding characters.
          */
-        //pad_length = len % 3;
         pad_length = (8 * len) % 6;
         pad_length = pad_length == 0 ? 0 : (6 - pad_length)/2;
 
-        result_len = len * 4 / 3;
+        /*
+         * Next, we allocate memory for the result string.  To find the number
+         * of 6-bit bytes we need, we multiply the number of source bytes by 8
+         * and (integer) divide by 6. If we need to do any padding, that means
+         * we need another 6-bit byte for the leftover bits. We'll also need to
+         * allocate for the padding characters.
+         */
+        result_len = len * 8 / 6;
         result_len += (pad_length ? 1 : 0) + pad_length;
 
         if ((result = (uint8_t *)malloc(result_len + 1)) == NULL)
                 exit(-1);
 
-        tmp1 = 0;
-        tmp2 = 0;
+        /*
+         * We can view the encoding of the src data as 3 cases which cycle over
+         * the bytes:
+         *
+         *   Case 0: Start
+         *   -------------
+         *   Here, the top 6-bits of the first byte are downshifted 2 to form
+         *   then ext encoded char. The leftover 2 bits are upshifted by 4 to be
+         *   used as part of the next encoded byte. 
+         *
+         *   Case 1: Middle
+         *   --------------
+         *   Here, the 2 bits from Case 0 are added to the first 4 bits of the
+         *   current byte (which are downshifted 4) to identify the next
+         *   encoding char. The remaining 4 bits of the current byte are
+         *   upshifted 2 to be used as part of the next encoded byte.
+         *
+         *   Case 2: End
+         *   -----------
+         *   Here, the 4 bits from Case 1 are added to the first 2 bits of the
+         *   current byte (downshifted 6) to identify the next encoding char.
+         *   This leaves exactly 6 bits from the current byte which are used to
+         *   identify one more encoding char.
+         */
+        leftover = 0;
         res_index = 0;
         for (i = 0; i < len; i++) {
                 cur = src[i];
-                /*
-                 * TODO: Describe the three cases
-                 */
                 switch (i % 3) {
                         case 0:
                                 result[res_index++] = digits64[cur >> 2];
-                                tmp1 = (0x3 & cur) << 4;
+                                leftover = (0x3 & cur) << 4;
                                 break;
 
                         case 1:
-                                result[res_index++] = digits64[tmp1 + (cur >> 4)];
-                                tmp2 = (0xF & cur) << 2;
-                                tmp1 = 0;
+                                result[res_index++] =
+                                                digits64[leftover + (cur >> 4)];
+                                leftover = (0xF & cur) << 2;
                                 break;
 
                         case 2:
-                                result[res_index++] = digits64[tmp2 + (cur >> 6)];
+                                result[res_index++] =
+                                                digits64[leftover + (cur >> 6)];
                                 result[res_index++] = digits64[0x3F & cur];
-                                tmp1 = 0;
-                                tmp2 = 0;
+                                leftover = 0;
                                 break;
                 }
         }
 
-        // Handle left over
-        switch ((i - 1) % 3) {
-                case 0:
-                        result[res_index++] = digits64[tmp1];
-                        break;
 
-                case 1:
-                        result[res_index++] = digits64[tmp2];
-                        break;
-        }
+        /*
+         * If there are any leftover bits, they will already have been shifted
+         * appropriately, so we can add the next encoding char directly.
+         */
+        if (leftover)
+                result[res_index++] = digits64[leftover];
 
-        /* Add padding */
+        /*
+         * The last step is to add the padding characters (if needed)
+         */
         for (i = 0; i < pad_length; i++) {
                 result[res_index++] = PADDING;
         }
 
-        /* Terminate string */
+        /*
+         * Don't forget to terminate the string!
+         */
         result[res_index++] = '\0';
 
         *dst = result;
